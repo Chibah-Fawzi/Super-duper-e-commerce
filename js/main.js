a35c28da22a277f4c9bce4a3d0802bb17fff7f9a
chibah-fawzi <fawzichibah@gmail.com>
2022-10-28 12:45:00 +0100

    fixed

--- js/main.js
+++ js/main.js
@@ -1,32 +1,51 @@
-var names = ["chocolat", "cheese", "kda mena", "halal menthe", "kinder pingui", "Zitoune", "Mechoui f chbab", "Kebch", "Rolex"]
-var prices = [12, 8, 7, 5, 15, 300, 4, 2, 1000]
-var descriptions = ["aaaa", "bbb", "vvvv", "cccccc", "chocolat", "cheese", "kda mena", "halal menthe", "kinder pingui"]
-var images = ['/img/image.jpg', '/img/img.jpg',]
+var names = [
+  "chocolat",
+  "cheese",
+  "kda mena",
+  "halal menthe",
+  "kinder pingui",
+  "Zitoune",
+  "Mechoui f chbab",
+  "Kebch",
+  "Rolex",
+];
+var prices = [12, 8, 7, 5, 15, 300, 4, 2, 1000];
+var descriptions = [
+  "aaaa",
+  "bbb",
+  "vvvv",
+  "cccccc",
+  "chocolat",
+  "cheese",
+  "kda mena",
+  "halal menthe",
+  "kinder pingui",
+];
+var images = ["./img/image.jpg", "/img/img.jpg"];
 
-var products = []
-var cart = []
+var products = [];
+var cart = [];
 
-
-var displayHtml = ""
+var displayHtml = "";
 
 for (let index = 0; index < names.length; index++) {
-    const name = names[index];
-    const price = prices[index];
-    const description = descriptions[index]
-    const image = images[index]
+  const name = names[index];
+  const price = prices[index];
+  const description = descriptions[index];
+  const image = images[index];
 
-    var product = {
-        id: index,
-        name,
-        description,
-        price,
-        image,
-        quantity: 1
-    }
-    products.push(product);
+  var product = {
+    id: index,
+    name,
+    description,
+    price,
+    image,
+    quantity: 1,
+  };
+  products.push(product);
 
-    /*html */
-    displayHtml += `
+  /*html */
+  document.getElementById("products").innerHTML += `
                 <div class="col-4">
                         <div class="card mb-4 box-shadow">
                             <img class="card-img-top"
@@ -44,37 +63,34 @@ for (let index = 0; index < names.length; index++) {
                                 </div>
                             </div>
                         </div>
-                </div>`
+                </div>`;
 }
 
-document.getElementById("products").innerHTML = displayHtml;
 document.getElementById("quantity").innerHTML = cart.length;
 
-
-
 function addingPrices(price, qty) {
-    return price * qty
+  return price * qty;
 }
 
 function addToCart(index) {
-    const currentProduct = products[index]
-    var displayHTML = document.getElementById('cartItems').innerHTML
+  const currentProduct = products[index];
+  var displayHTML = document.getElementById("cartItems").innerHTML;
 
-    const found = cart.find(produit => produit.id == currentProduct.id)
+  const found = cart.find((produit) => produit.id == currentProduct.id);
 
-    if (found) {
-        cart.forEach(produit => {
-            if (produit.id == found.id) {
-                produit.quantity++
-                document.getElementById("Quantity" + produit.id).innerHTML = produit.quantity
-                // product.price += product.price
-                // document.getElementById("Price" + produit.id).innerHTML = product.price
-            }
-        });
-    } else {
-
-        cart.push(currentProduct)
-        displayHTML += `
+  if (found) {
+    cart.forEach((produit) => {
+      if (produit.id == found.id) {
+        produit.quantity++;
+        document.getElementById("Quantity" + produit.id).innerHTML =
+          produit.quantity;
+        // product.price += product.price
+        // document.getElementById("Price" + produit.id).innerHTML = product.price
+      }
+    });
+  } else {
+    cart.push(currentProduct);
+    displayHTML += `
                                 <tr id="prod-${currentProduct.id}">
                                     <td>${currentProduct.id}</th>
                                     <td>${currentProduct.name}</td>
@@ -83,34 +99,33 @@ function addToCart(index) {
                                     <td>
                                         <div onclick="removeItem(${currentProduct.id})" class="btn btn-danger">Delete</div>
                                     </td>
-                            </tr>`
-        document.getElementById('cartItems').innerHTML = displayHTML
-        document.getElementById("quantity").innerHTML = cart.length;
-    }
-
+                            </tr>`;
+    document.getElementById("cartItems").innerHTML = displayHTML;
+    document.getElementById("quantity").innerHTML = cart.length;
+  }
 }
 
-const form = document.getElementById('form-group');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const name = document.getElementById('nameInput').value
-    const price = document.getElementById('price').value
-    const description = document.getElementById('description').value
+const form = document.getElementById("form-group");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const name = document.getElementById("nameInput").value;
+  const price = document.getElementById("price").value;
+  const description = document.getElementById("description").value;
 
-    var newProduct = {
-        id: products.length,
-        name,
-        description,
-        price,
-        quantity: 1
-    }
+  var newProduct = {
+    id: products.length,
+    name,
+    description,
+    price,
+    quantity: 1,
+  };
 
-    if (name == "" || description == "" || price == "") {
-        alert('add content!');
-    }
-    products.push(newProduct)
-    console.log(products)
-    displayHtml += `
+  if (name == "" || description == "" || price == "") {
+    alert("add content!");
+  }
+  products.push(newProduct);
+  console.log(products);
+  displayHtml += `
                 <div class="col-4">
                         <div class="card mb-4 box-shadow">
                             <img class="card-img-top"
@@ -128,21 +143,19 @@ form.addEventListener('submit', function (e) {
                                 </div>
                             </div>
                         </div>
-                </div>`
-    document.getElementById("products").innerHTML = displayHtml;
-
-})
+                </div>`;
+  document.getElementById("products").innerHTML = displayHtml;
+});
 
 function removeItem(id) {
-    cart = cart.filter((element) => element.id != id)
+  cart = cart.filter((element) => element.id != id);
 
-    if (products[id].quantity > 1) {
-        products[id].quantity--;
-        document.getElementById("Quantity" + products[id].id).innerHTML = products[id].quantity;
-    } else {
-        document.getElementById("prod-" + id).remove()
-        document.getElementById("quantity").innerHTML = cart.length;
-    }
+  if (products[id].quantity > 1) {
+    products[id].quantity--;
+    document.getElementById("Quantity" + products[id].id).innerHTML =
+      products[id].quantity;
+  } else {
+    document.getElementById("prod-" + id).remove();
+    document.getElementById("quantity").innerHTML = cart.length;
+  }
 }
-
-
